19f71a968d5e692efe73c43bc4b68416f4b0e420
Aidan Lee <Aidan@Aidans-MacBook-Pro.local>
2020-04-06 16:27:18 -0400

    add functionality

--- js/main.js
+++ js/main.js
@@ -1,14 +1,43 @@
-const vm = new Vue({
 
-    data: {
-        
-    },
+(() => {
+    const vm = new Vue({
+        el: '#app',
+        data: {
+            message: "how are ya"
+        },
 
-    created: function() {
-        console.log("parent is live");
-    },
+        created: function() {
+            console.log("parent is live");
+        },
 
-    methods: {
+        methods: {
+            openMovie() {
+                document.querySelector(".movie-lightbox").classList.add('show-lightbox');
+            }, 
 
-    }
-}).$mount("#app");
+            openTv() {
+                document.querySelector(".tv-lightbox").classList.add('show-lightbox');
+            }, 
+
+            openMusic() {
+                document.querySelector(".music-lightbox").classList.add('show-lightbox');
+            }, 
+
+            closeLightbox() {
+                document.querySelector(".lightbox").classList.remove('show-lightbox');
+            },
+
+            closeMovie() {
+                document.querySelector(".movie-lightbox").classList.remove('show-lightbox');
+            },
+
+            closeTv() {
+                document.querySelector(".tv-lightbox").classList.remove('show-lightbox');
+            },
+
+            closeMusic() {
+                document.querySelector(".music-lightbox").classList.remove('show-lightbox');
+            },
+        }
+    });
+})();
